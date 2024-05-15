131694985550ce5acaa6ea89b916dcf936758f11
Jokwanhee <jonotch1@naver.com>
2024-05-15 19:10:50 +0900

    Add libs.versions.toml & migrate basic vcs to version catalog

--- kwanhee/AndroidStudy/app/build.gradle.kts
+++ kwanhee/AndroidStudy/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin)
 }
 
 android {
@@ -36,12 +36,11 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
+    implementation(libs.junit)
+    implementation(libs.androidx.test.ext)
+    implementation(libs.androidx.test.espresso)
 }
